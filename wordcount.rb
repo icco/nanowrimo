dbbc3e21c3ce8e77bc8f28051a788ef8a0900ec2
Nat Welch <nat@natwelch.com>
2011-11-01 22:06:42 -0700

    Nice number formatting.

--- wordcount.rb
+++ wordcount.rb
@@ -14,18 +14,25 @@ def help
   help
 end
 
+class Integer
+  def nice
+    return self.to_s.gsub(/(\d)(?=(\d\d\d)+(?!\d))/, "\\1,")
+  end
+end
+
 def count str
   date = Time.now
   count = str.split(/\S+/).size
   day = date.day
   needed = WORDS_PER_DAY * day
   remaining = needed - count
+  month_rem = TOTAL_WORDS-count
 
   puts "#{date.strftime("%B %e, %Y")}:"
-  puts "  Words typed:  #{count}"
-  puts "  Words needed: #{needed}"
-  puts "  Left Today:   #{remaining}"
-  puts "  Left Total:   #{TOTAL_WORDS-count}"
+  puts "  Words typed:  #{count.nice}"
+  puts "  Words needed: #{needed.nice}"
+  puts "  Left Today:   #{remaining.nice}"
+  puts "  Left Total:   #{month_rem.nice}"
 
   return count
 end
